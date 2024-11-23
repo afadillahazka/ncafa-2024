8e89e1d81fc4c91bbc226061a09e1cef14931e7b
Aditya Fadillah Azka <afadillahazka@gmail.com>
2024-11-23 19:37:31 +0700

    add: 2025 year

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		zIndex: {
+			'999': 999,
+			'0': 0,
+		   '10': 10,
+		   '20': 20,
+		   '30': 30,
+		   '40': 40,
+		   '50': 50,
+		   '25': 25,
+		   '50': 50,
+		   '75': 75,
+		   '100': 100,
+			'auto': 'auto',
+		  },
 		extend: {
 			backgroundImage: {
 				'ncafa': "linear-gradient(to bottom,#078193 10%,#2e4696 90%)",
